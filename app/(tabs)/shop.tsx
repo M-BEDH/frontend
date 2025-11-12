9f16804a47304b2a78128d0c8257afa4db6bce5e
M-BEDH <https://github.com/M-BEDH>
2025-11-12 11:18:18 +0100

    initialisation - page home profile et shop + import iconsVector

---
+++ app/(tabs)/shop.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Shop = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>ShopScreen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Shop
+
+const styles = StyleSheet.create({})
